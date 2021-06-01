8305d5e08b1024f31be507de662764a3f5120078
jyp1793 <jyp1793@gmail.com>
2021-06-01 17:53:16 +0900

    day04

--- 01basic/script01.js
+++
@@ -1,2 +0,0 @@
-var d = 10;
-document.write(d);
---
+++ 03side_bar/sidebar.js
@@ -0,0 +1,29 @@
+var sidenav = document.querySelector(".sidenav");
+var menuBtn = document.querySelector(".menuBtn");
+
+menuBtn.addEventListener("click", onSidenav);
+
+function onSidenav() {
+    // console.log(sidenav.style.width);
+
+    var btn = sidenav.style.width;
+    // console.log(window.innerWidth); 브라우저의 안쪽넓이
+
+    if(window.innerWidth <= 767) { //모바일환경
+
+        if(btn == "" || btn == "0px") {
+            sidenav.style.width = "200px";
+        } else {
+            sidenav.style.width = "0px";
+        }
+        
+    } else { //웹환경
+        if(btn == "" || btn == "200px") {
+            sidenav.style.width = "0px";
+        } else {
+            sidenav.style.width = "200px";
+        }
+
+    }
+
+}
