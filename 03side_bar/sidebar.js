71ead997152d4df64c84ae8fb283eb6e05c60f8c
jyp1793 <jyp1793@gmail.com>
2021-06-07 00:45:53 +0900

    Day04

--- 03side_bar/sidebar.js
+++ 03side_bar/sidebar.js
@@ -4,20 +4,19 @@ var menuBtn = document.querySelector(".menuBtn");
 menuBtn.addEventListener("click", onSidenav);
 
 function onSidenav() {
-    // console.log(sidenav.style.width);
-
     var btn = sidenav.style.width;
-    // console.log(window.innerWidth); 브라우저의 안쪽넓이
-
-    if(window.innerWidth <= 767) { //모바일환경
-
+    // console.log(window.innerWidth ); //브라우저의 안쪽넓이
+    
+    if(window.innerWidth <= 767) { //모바일
+        
         if(btn == "" || btn == "0px") {
             sidenav.style.width = "200px";
         } else {
             sidenav.style.width = "0px";
         }
-        
+
     } else { //웹환경
+
         if(btn == "" || btn == "200px") {
             sidenav.style.width = "0px";
         } else {
@@ -25,5 +24,8 @@ function onSidenav() {
         }
 
     }
+
+
+
 
 }
